70e514a3b04e3cfde1f8482620a935fe80f4caa6
Jimmy Krag <jimmy@kragenet.dk>
2016-09-12 15:26:29 +0000

    quickstart.txt edited online with Bitbucket

---
+++ README.txt
@@ -0,0 +1,42 @@
+===============================================================
+django-sphinxdoc – Embed your Sphinx docs into your Django site
+===============================================================
+
+This Django application allows you to integrate any `Sphinx
+<http://sphinx.pocoo.org/>`_ documentation directly into your Django powered
+website instead of just serving the static files.
+
+Django-sphinxdoc can handle multiple Sphinx projects and offers a `Haystack
+<http://haystacksearch.org/>`_ powered search. Future versions will enable
+comments and add RSS feeds.
+
+
+Requirements
+------------
+
+This app requires Django >= 1.4.2, Sphinx >= 1.0 and Haystack >= 2.1.
+
+
+Installation
+------------
+
+Just use `PIP <http://pypi.python.org/pypi/pip>`_:
+
+.. sourcecode:: bash
+
+    $ pip install django-sphinxdoc
+
+If you want the lates development version, isntall it from Bitbucket:
+
+.. sourcecode:: bash
+
+    $ pip install https://ssc@bitbucket.org/sscherfke/django-sphinxdoc
+    $ # or
+    $ hg clone ssh://hg@bitbucket.org/sscherfke/django-sphinxdoc
+    $ pip install -e django-sphinxdoc
+
+
+Usage
+-----
+
+The Documentation can be found in the *docs/* directory.
---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='django-sphinxdoc',
+    version='1.4.0',
+    author='Stefan Scherfke',
+    author_email='stefan at sofa-rockers.org',
+    description='Easily integrate Sphinx documentation into your website.',
+    long_description=(open('README.txt').read() + '\n\n' +
+                      open('CHANGES.txt').read() + '\n\n' +
+                      open('AUTHORS.txt').read()),
+    url='https://bitbucket.org/sscherfke/django-sphinxdoc/',
+    license='MIT',
+    install_requires=[
+        'Django>=1.4.2',
+        'django-haystack>=2.1',
+    ],
+    packages=find_packages(exclude=['*.tests', '*.tests.*']),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ],
+)
